31db9b62956b43065606470f28a8b9ede3d05de3
Paul Simon K <paulsimonk2@gmail.com>
2022-08-29 14:22:12 +0530

    my changes

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"simpleapp/controller"
+)
+
+func main() {
+
+	controller.Master()
+	// Then we create a new server and start listening for incoming requests with the http.ListenAndServe() function, passing in our servemux for it to match requests against as the second parameter.
+	fmt.Println("Server serving...")
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+
+}
